b8146c4f96c852bfe28a1619cca7430b123601cf
khanoftexas <khanoftexas@gmail.com>
2019-04-17 23:36:23 -0500

    Ongoing changes

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy import func
-import numpy as np
-import pandas as pd
+
+# import numpy as np
+# import pandas as pd
 
 import datetime
 
